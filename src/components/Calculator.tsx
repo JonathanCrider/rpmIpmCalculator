332d90134699629670ccdcecaf5ca455e65ee81f
JonathanCrider <13684007+JonathanCrider@users.noreply.github.com>
2024-10-25 12:38:46 -0400

    [FEAT] handles arrow and enter key events

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,4 +1,4 @@
-import { useReducer } from 'react'
+import { useReducer, useRef } from 'react'
 import { SET_DIA, SET_SFM, SET_CPT, SET_NUM_TEETH, SET_RPM, SET_IPM, type CalculatorState, type Action } from './Calculator.types'
 import './Calculator.css'
 
@@ -30,6 +30,11 @@ function Calculator() {
     rpm: 0,
     ipm: 0
   })
+  
+  const refDia = useRef<HTMLInputElement>(null)
+  const refSfm = useRef<HTMLInputElement>(null)
+  const refCpt = useRef<HTMLInputElement>(null)
+  const refTeeth = useRef<HTMLInputElement>(null)
 
   function results () {
     const { sfm, dia, numTeeth, cpt } = state
@@ -39,10 +44,16 @@ function Calculator() {
     return
   }
 
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
+  const handleKeyDown = (
+    e: React.KeyboardEvent<HTMLDivElement>,
+    previous: React.RefObject<HTMLInputElement> | null,
+    next: React.RefObject<HTMLInputElement> | null
+  ) => {
     console.log(e.key)
     if (e.key === 'ArrowUp' || e.key === 'ArrowDown') {
       e.preventDefault()
+      if (e.key === 'ArrowUp' && previous?.current) previous.current.focus()
+      else if (next?.current) next.current.focus() 
     }
     if (e.key === 'Enter') {
       results()
@@ -51,7 +62,7 @@ function Calculator() {
 
   return (
     <>
-      <div className='calculator' onKeyDown={handleKeyDown}>
+      <div className='calculator'>
         <div className='row'>
           <label>RPM</label>
           <input className='results' name='rpm' type='number' placeholder='RPM' disabled value={state.rpm || undefined}/>
@@ -61,16 +72,16 @@ function Calculator() {
           <input className='results' name='ipm' type='number' placeholder='IPM' disabled value={state.ipm || undefined}/>
         </div>
         <div className='row'>
-          <input name='dia' type='number' placeholder='diameter' onChange={e => dispatch({ type: SET_DIA, payload: +e.target.value})}/>
+          <input name='dia' type='number' placeholder='diameter' ref={refDia} onKeyDown={(e) => handleKeyDown(e, refTeeth, refSfm)} onChange={e => dispatch({ type: SET_DIA, payload: +e.target.value})}/>
         </div>
         <div className='row'>
-          <input name='sfm' type='number' placeholder='SFM' onChange={e => dispatch({ type: SET_SFM, payload: +e.target.value})}/>
+          <input name='sfm' type='number' placeholder='SFM' ref={refSfm} onKeyDown={(e) => handleKeyDown(e, refDia, refCpt)} onChange={e => dispatch({ type: SET_SFM, payload: +e.target.value})}/>
         </div>
         <div className='row'>
-          <input name='cpt' type='number' placeholder='CPT' onChange={e => dispatch({ type: SET_CPT, payload: +e.target.value})}/>
+          <input name='cpt' type='number' placeholder='CPT' ref={refCpt} onKeyDown={(e) => handleKeyDown(e, refSfm, refTeeth)} onChange={e => dispatch({ type: SET_CPT, payload: +e.target.value})}/>
         </div>
         <div className='row'>
-          <input name='teeth' type='number' placeholder='number of teeth' onChange={e => dispatch({ type: SET_NUM_TEETH, payload: +e.target.value})}/>
+          <input name='teeth' type='number' placeholder='number of teeth' ref={refTeeth} onKeyDown={(e) => handleKeyDown(e, refCpt, refDia)} onChange={e => dispatch({ type: SET_NUM_TEETH, payload: +e.target.value})}/>
         </div>
         <div className='row'>
           <button onClick={() => results()}>
